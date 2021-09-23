65388f42219346344d2c01d0d3fbaa9ca2ce7205
davixe7 <davidguilarte7@gmail.com>
2021-09-23 00:11:37 -0400

    Basic dues module done

---
+++ database/migrations/2021_09_23_040713_create_dues_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dues', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('date');
+            $table->integer('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dues');
+    }
+}
