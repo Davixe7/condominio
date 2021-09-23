f4ce2dcba7f1d0a452b43037c31cc1c644a8e921
davixe7 <davidguilarte7@gmail.com>
2021-09-23 00:58:32 -0400

    Vuetify installed successfully

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+import VApp from 'vuetify/lib/components/VApp'
+import VMain from 'vuetify/lib/components/VMain'
+
+Vue.use(Vuetify)
+
+const app = new Vue({
+  vuetify: new Vuetify({}),
+  components: {
+    VApp, VMain
+  },
+  el: '#app'
+})
